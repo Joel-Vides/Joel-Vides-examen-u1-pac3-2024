a5120c9155088e71959cd1c3c81df3511e828e36
Joel-Vides <joelvides00@gmail.com>
2024-10-17 17:57:53 -0600

    init

---
+++ js/app.js
@@ -0,0 +1,10 @@
+alert('Comparación de cadenas');
+const texto = prompt('Ingresar el texto: ');
+
+const texto2 = prompt('Ingresar el Segundo Texto: ');
+
+if(texto===texto2){
+    alert('Los Textos son Iguales')
+}else{
+    alert('Los Textos no son Iguales!')
+}
